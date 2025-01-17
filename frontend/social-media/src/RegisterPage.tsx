01ce0f9d04f3e5e839e70065015687972ce80ce8
Hurishihara <azgelo21@gmail.com>
2025-01-17 11:53:14 +0800

    add CORS middleware to package.json; implement registration form with API call in RegisterPage component

--- frontend/social-media/src/RegisterPage.tsx
+++ frontend/social-media/src/RegisterPage.tsx
@@ -1,10 +1,29 @@
-import { Box, Card, Flex, Input, Link, Separator, Stack, Text } from "@chakra-ui/react"
+import { useState } from "react"
+import axios from "axios"
+import { Box, Card, Flex, Input, Stack, } from "@chakra-ui/react"
 import { Button } from "./src/components/ui/button"
 import { PasswordInput } from "./src/components/ui/password-input"
 
 
 const RegisterPage = () => {
  
+  const api = 'https://localhost:3000/api/register'
+  const [ username, setUsername ] = useState<string>('')
+  const [ email, setEmail ] = useState<string>('')
+  const [ password, setPassword ] = useState<string>('')
+
+  const handleRegister = async (e: any) => {
+    e.preventDefault()
+
+    try {
+      const response = await axios.post(api, {username, email, password})
+      console.log(response.data.message)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <Flex justifyContent='center' alignItems='center' h='90vh'>
     <Box w='400px'>
@@ -14,10 +33,12 @@ const RegisterPage = () => {
       </Card.Body>
       <Card.Body>
       <Stack gap='1rem' direction='column'>
-        <Input placeholder='Username' />
-        <Input placeholder='Email' />
-        <PasswordInput placeholder='Password' />
-        <Button>Sign Up</Button>
+        <form id='register-form' onSubmit={handleRegister} method="post">
+          <Input placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+          <Input placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <PasswordInput placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        </form>
+        <Button type='submit' form='register-form' >Sign Up</Button>
       </Stack>
       </Card.Body>
     </Card.Root>
