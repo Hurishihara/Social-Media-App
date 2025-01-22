70344f9b6b324688a04e81a285a9b020a387771c
Hurishihara <azgelo21@gmail.com>
2025-01-22 17:04:21 +0800

    installed cloudinary

---
+++ backend/src/db/cloudinary.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+import { v2 as cloudinary } from "cloudinary";
+
+export const imageDb = cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+})
